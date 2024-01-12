b70abe58ffd0483cb05c3ffb2125bb69ba7bf9b4
Diogo Pires <dpirespereira86@gmail.com>
2024-01-12 08:49:06 -0300

    Inicial

--- WareHouse/urls.py
+++ WareHouse/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for WareHouse project.
+URL configuration for Warehouse project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from Armazem.urls import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/produtos/', include('Produto.urls')),
+    path('api/v1/armazem/', include(router.urls)),
 ]
